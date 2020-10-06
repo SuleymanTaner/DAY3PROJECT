8da6309dd51f9b46c4a263c701d41de4ccb331eb
SuleymanTaner <suleymantaner44@gmail.com>
2020-10-06 20:46:24 +0100

    Added new file for awesome feature

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,10 @@
+public class AwesomeTest {
+
+    // green means it's untracked
+    // green it's under staging area
+    // when the color is normal, it means it's committed no changes
+
+
+
+
+}
