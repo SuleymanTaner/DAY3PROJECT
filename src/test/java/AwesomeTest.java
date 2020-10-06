a93b2e9e9c1893059f93fd827478e65ed203a607
SuleymanTaner <suleymantaner44@gmail.com>
2020-10-06 20:59:12 +0100

    added MORE awesome files

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -2,9 +2,13 @@ public class AwesomeTest {
 
     // green means it's untracked
     // green it's under staging area
+    // grey in .idea folder means it's ignored
     // when the color is normal, it means it's committed no changes
 
+    // i just pulled the new change from Github that i did not have locally
+    // and now my project is in sync with remote repository
 
+    // Now I mande more awesome changes and i am about to push this back to Github
 
 
 }
